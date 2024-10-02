ef79ab3329c48849c9277d5b938315c8c6f6d3f2
Kuldeep <kuldeep363rawat@gmail.com>
2024-10-02 16:52:30 +0530

    FIX: reciver name issue fixed

--- src/app/components/home/VideoCall.tsx
+++ src/app/components/home/VideoCall.tsx
@@ -10,7 +10,7 @@ const VideoCall = () => {
           <Video videoRef={socketContext?.myVideo} type="my" user={socketContext?.name} />
           {
             socketContext?.callAccepted && !socketContext.callEnded ?
-            <Video videoRef={socketContext?.userVideo} type="caller" user={socketContext?.call?.name} />
+            <Video videoRef={socketContext?.userVideo} type="caller" user={socketContext?.call?.toName} />
             :
             null
           }
